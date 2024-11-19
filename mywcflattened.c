862ae519b95df068a94f9055173ac36d6e3609ce
jeffkxu <jeffrey.xu@princeton.edu>
2024-11-19 10:59:38 -0500

    flattened it

---
+++ mywcflattened.c
@@ -0,0 +1,61 @@
+/*--------------------------------------------------------------------*/
+/* mywc.c                                                             */
+/* Author: Bob Dondero                                                */
+/*--------------------------------------------------------------------*/
+
+#include <stdio.h>
+#include <ctype.h>
+
+/*--------------------------------------------------------------------*/
+
+/* In lieu of a boolean data type. */
+enum {FALSE, TRUE};
+
+/*--------------------------------------------------------------------*/
+
+static long lLineCount = 0;      /* Bad style. */
+static long lWordCount = 0;      /* Bad style. */
+static long lCharCount = 0;      /* Bad style. */
+static int iChar;                /* Bad style. */
+static int iInWord = FALSE;      /* Bad style. */
+
+/*--------------------------------------------------------------------*/
+
+/* Write to stdout counts of how many lines, words, and characters
+   are in stdin. A word is a sequence of non-whitespace characters.
+   Whitespace is defined by the isspace() function. Return 0. */
+
+int main(void)
+{
+
+    whileLoop: 
+
+        if ((iChar = getchar()) == EOF) goto endWhileLoop;
+            lCharCount++;
+            if (!isspace(iChar)) goto notSpace;
+                if (!iInWord) goto notInWord;
+                    lWordCount++;
+                    iInWord = FALSE;
+                notInWord:
+                goto newlineChecker;
+
+            notSpace:
+                if (iInWord) goto newlineChecker;
+                    iInWord = TRUE;
+            
+            newlineChecker:
+                if (iChar != '\n') goto whileLoop;
+                    lWordCount++;
+            
+            goto whileLoop;
+
+    endWhileLoop:
+
+    if (!iInWord) goto printStatement;
+        lWordCount++;
+
+    printStatement:
+        printf("%7ld %7ld %7ld\n", lLineCount, lWordCount, lCharCount);
+
+    return 0;
+}
